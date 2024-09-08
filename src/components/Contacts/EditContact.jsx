5848a818aeb1f1851acbf10e3ce596fc6ce1ecca
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-09-08 22:47:11 +0330

    create Edit contacts

---
+++ src/components/Contacts/EditContact.jsx
@@ -0,0 +1,118 @@
+import { useEffect, useContext } from "react";
+
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import { useImmer } from "use-immer";
+import { toast } from "react-toastify";
+
+import { ContactContext } from "../../context/contactContext";
+import { getContact, updateContact } from "../../services/contactService";
+import { Spinner } from "../";
+import { COMMENT, ORANGE, PURPLE } from "../../helpers/colors";
+import { contactSchema } from "../../validations/contactValidation";
+
+const EditContact = () => {
+  const { contactId } = useParams();
+  const { setContacts, setFilteredContacts, loading, setLoading, groups } =
+    useContext(ContactContext);
+
+  const navigate = useNavigate();
+
+  const [contact, setContact] = useImmer({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data: contactData } = await getContact(contactId);
+
+        setLoading(false);
+        setContact(contactData);
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const submitForm = async (values) => {
+    try {
+      setLoading(true);
+      const { data, status } = await updateContact(values, contactId);
+
+      if (status === 200) {
+        setLoading(false);
+
+        toast.info("مخاطب با موفقیت ویرایش شد", { icon: "✅" });
+
+        setContacts((draft) => {
+          const contactIndex = draft.findIndex(
+            (c) => c.id === parseInt(contactId)
+          );
+          draft[contactIndex] = { ...data };
+        });
+        setFilteredContacts((draft) => {
+          const contactIndex = draft.findIndex(
+            (c) => c.id === parseInt(contactId)
+          );
+          draft[contactIndex] = { ...data };
+        });
+
+        navigate("/contacts");
+      }
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <section className="p-3">
+            <div className="container">
+              <div className="row my-2">
+                <div className="col text-center">
+                  <p className="h4 fw-bold" style={{ color: ORANGE }}>
+                    ویرایش مخاطب
+                  </p>
+                </div>
+              </div>
+              <hr style={{ backgroundColor: ORANGE }} />
+              <div
+                className="row p-2 w-75 mx-auto align-items-center"
+                style={{ backgroundColor: "#44475a", borderRadius: "1em" }}
+              >
+                <div className="col-md-8">
+                  
+                </div>
+                <div className="col-md-4">
+                  <img
+                    src={contact.photo}
+                    className="img-fluid rounded"
+                    style={{ border: `1px solid ${PURPLE}` }}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="text-center mt-1">
+              <img
+                src={require("../../assets/man-taking-note.png")}
+                height="300px"
+                style={{ opacity: "60%" }}
+              />
+            </div>
+          </section>
+        </>
+      )}
+    </>
+  );
+};
+
+export default EditContact;
