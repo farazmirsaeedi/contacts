1b07e77830de084ac81dfd59b2a4427a53803eb0
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-09-08 23:00:51 +0330

    complete http requests

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -17,3 +17,37 @@ export const getContact = (contactId) => {
   return axios.get(url);
 };
 
+// @desc  Get All Groups
+// @route GET http://localhost:9000/groups
+export const getAllGroups = () => {
+  const url = `${SERVER_URL}/groups`;
+  return axios.get(url);
+};
+
+// @desc  Get Group Name With Group ID
+// @route GET http://localhost:9000/groups/:groupId
+export const getGroup = (groupId) => {
+  const url = `${SERVER_URL}/groups/${groupId}`;
+  return axios.get(url);
+};
+
+// @desc  Create New Contact
+// @route POST http://localhost:9000/contacts
+export const createContact = (contact) => {
+  const url = `${SERVER_URL}/contacts`;
+  return axios.post(url, contact);
+};
+
+// @desc  Update Contact
+// @route PUT http://localhost:9000/contacts/:contactId
+export const updateContact = (contact, contactId) => {
+  const url = `${SERVER_URL}/contacts/${contactId}`;
+  return axios.put(url, contact);
+};
+
+// @desc  Delete Contact
+// @route DELETE http://localhost:9000/contacts/:contactId
+export const deleteContact = (contactId) => {
+  const url = `${SERVER_URL}/contacts/${contactId}`;
+  return axios.delete(url);
+};
